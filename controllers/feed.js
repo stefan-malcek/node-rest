8dd03b5a7a6ba631a0741558f466cf1fd0cd1672
Stefan Malcek <stefan.malcek1@gmail.com>
2019-09-16 22:27:03 +0200

    Add basic validation.

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,15 +1,43 @@
+const { validationResult } = require('express-validator');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }]
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.jpg',
+        creator: {
+          name: 'Name'
+        },
+        createdAt: new Date()
+      }
+    ]
   });
 };
 
 exports.createPost = (req, res, next) => {
-  const title = req.body.title;
-  const content = req.body.content;
-  // Create post in db
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      message: 'Validation failed. Check the errors to see more details.',
+      errors: errors.array()
+    });
+  }
+
+  const { title, content } = req.body;
+
   res.status(201).json({
     message: 'Post created successfully!',
-    post: { id: new Date().toISOString(), title: title, content: content }
+    post: {
+      _id: new Date().toISOString(),
+      title: title,
+      content: content,
+      creator: {
+        name: 'Name'
+      },
+      createdAt: new Date()
+    }
   });
 };
